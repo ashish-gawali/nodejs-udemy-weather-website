64c97fda351aa8c9813d8394bdaa45fcf046df59
Ashish Gawali <gawaliaa15.extc@coep.ac.in>
2020-07-02 11:23:44 +0530

    add about text. And changing to celsius.

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,7 +1,7 @@
 const request = require('request')
 
 const forecast = (latitude, longitude, callback) => {
-    const url = 'http://api.weatherstack.com/current?access_key=0f2006271b70e7a422f411081f1d29f7&query=' + latitude + ',' + longitude + '&units=f';
+    const url = 'http://api.weatherstack.com/current?access_key=0f2006271b70e7a422f411081f1d29f7&query=' + latitude + ',' + longitude + '&units=m';
     request({ url, json: true }, (error, { body }) => {
         if (error) {
             callback('Unable to connect to the weather service!', undefined);
